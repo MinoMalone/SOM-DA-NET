3eaf8ae2ca8abc7b46ef0b8242d012c3abe3942c
MinoMalone <malonemino@gmail.com>
2026-09-04 13:43:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const posts = [
   nome: "MINO MALONE",
     titulo: "PASSOS",
     imagem: "Vectorify_1788519567658_c4.svg",
-    link: "FACEBOOK.COM",
+    link: "https://chatgpt.com/",
     data: "Ontem"
   },
   {
